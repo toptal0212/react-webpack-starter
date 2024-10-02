ab8c3ff9410a41297b89a72d05a82b4e6004a228
ToptalDev <proLove0212@gmail.com>
2024-10-02 17:00:52 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 import { Routing } from './routing.component';
 import './app/core/scss/style.scss';
 
+
 ReactDOM.render(<Routing />, document.getElementById('app'));
